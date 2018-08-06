399ab0c8c77f1792d6ec7dabe0f460b0dd1d75c7
master11360 <master113600@gmail.com>
2018-08-06 17:37:49 +0800

    Add test 'test_home_page_returns_correct_html'.

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from lists.views import home_page
 from django.core import urlresolvers
+from django.http import HttpRequest
 
 # Create your tests here.
 
@@ -12,3 +13,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = urlresolvers.resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith('</html>'))
