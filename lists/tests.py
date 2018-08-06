ebe31454ad19a69abd8d28cc60b77d7ec1c94a97
master11360 <master113600@gmail.com>
2018-08-06 17:25:49 +0800

    First unit test of url mapping, dummy view.

--- lists/tests.py
+++ lists/tests.py
@@ -2,10 +2,13 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from lists.views import home_page
+from django.core import urlresolvers
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = urlresolvers.resolve('/')
+        self.assertEqual(found.func, home_page)
