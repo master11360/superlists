6ec08fc2f1326ac8ef8a8d6b506f2a5a2aefe81a
master11360 <master113600@gmail.com>
2018-08-08 18:07:12 +0800

    Add List model. Let List be a foreign key of Item.

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.shortcuts import redirect
 from lists.models import Item
+from lists.models import List
 
 # Create your views here.
 
@@ -18,5 +19,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
