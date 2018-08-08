141bd9bf950395d8c548e9a204d6162caab7cd2a
master11360 <master113600@gmail.com>
2018-08-08 18:30:34 +0800

    Show list item for individual current user.

--- lists/tests.py
+++ lists/tests.py
@@ -55,7 +55,8 @@ class ListAndItemModelsTest(TestCase):
 
 class ListViewTest(TestCase):
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % list_.id)
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_items(self):
@@ -63,10 +64,25 @@ class ListViewTest(TestCase):
         Item.objects.create(text='item 1', list=list_)
         Item.objects.create(text='item 2', list=list_)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get('/lists/%d/' % list_.id)
 
         self.assertContains(response, 'item 1')
         self.assertContains(response, 'item 2')
+
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='item 1', list=correct_list)
+        Item.objects.create(text='item 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other item 1', list=other_list)
+        Item.objects.create(text='other item 2', list=other_list)
+
+        response = self.client.get('/lists/%d/' % correct_list.id)
+
+        self.assertContains(response, 'item 1')
+        self.assertContains(response, 'item 2')
+        self.assertNotContains(response, 'other item 1')
+        self.assertNotContains(response, 'other item 2')
 
 
 class NewListTest(TestCase):
@@ -84,4 +100,5 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'A new list item'}
         )
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % new_list.id)
